9ac42d1e20ec3eb029748c10e3efed08d46a4c2f
Kirill Kaidanov <k.kaidanov@gardatech.ru>
2025-04-16 15:05:49 +0300

    main: init file

---
+++ main.py
@@ -0,0 +1,12 @@
+def code_to_edit(a, b):
+    return a + b
+
+def main():
+    first = 5
+    second = 12
+    result = code_to_edit(first, second)
+    print(first, '+', second, '=', result)
+
+if __name__ == '__main__':
+    main()
+
