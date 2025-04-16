2f9e2ea66754040b737c340e8c0ef40f393aa8cd
Kirill Kaidanov <k.kaidanov@gardatech.ru>
2025-04-16 15:08:16 +0300

    main: edited file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def code_to_edit(a, b):
-    return a + b
+    return a - b
 
 def main():
-    first = 5
-    second = 12
+    first = 10
+    second = 112
     result = code_to_edit(first, second)
-    print(first, '+', second, '=', result)
+    print(first, '-', second, '=', result)
 
 if __name__ == '__main__':
     main()
